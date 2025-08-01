d20c2b13390ec6f78bf8814ad615874e29589970
Eddah kibet <eddahkibet20@gmail.com>
2025-08-01 14:26:52 +0300

    routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,27 @@
-
+import Directors from './pages/Directors';
+import Home from './pages/Home';
+import Actors from './pages/Actors';
+import Movies from './pages/Movie';
+import Errorpage from "./pages/Errorpage"
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <Errorpage />
+  },
+  {
+    path: "/directors",
+    element: <Directors/>,
+  },
+  {
+    path: "/actors",
+    element: <Actors/>,
+  },
+  {
+    path: "/movies",
+    element: <Movies/>,
+  }
+];
 
 export default routes;
